7b73ec5c5481c1de1b95a699d44365d6752402fe
asiwajudev <olanrewajuranda@gmail.com>
2020-02-23 19:21:26 +0100

    favicon and other logo links removed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import "bootstrap/dist/css/bootstrap.css";
-
 ReactDOM.render(<App />, document.getElementById("root"));
